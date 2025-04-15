b208c80c61f8a9def70cbcb443493011caf29482
Islam Kairgazin <islam.kairgazin@narxoz.kz>
2025-04-15 16:35:38 +0500

    Update Main.java

--- src/TechSupport/Main.java
+++ src/TechSupport/Main.java
@@ -1,14 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        SupportHandler faq = new FAQBotHandler();
-        SupportHandler junior = new JuniorSupportHandler();
-        SupportHandler senior = new SeniorSupportHandler();
+        Light light = new Light();
+        Thermostat thermostat = new Thermostat();
+        
+        Command lightOn = new TurnOnLightCommand(light);
+        Command setThermostat = new SetThermostatCommand(thermostat, 22);
 
-        faq.setNext(junior).setNext(senior);
+        SmartHomeRemoteControl remote = new SmartHomeRemoteControl();
+        remote.setCommand("lightOn", lightOn);
+        remote.setCommand("setThermostat", setThermostat);
 
-        String[] issues = {"password_reset", "refund_request", "account_ban", "unknown_bug"};
-        for (String issue : issues) {
-            faq.handle(issue);
-        }
+        remote.pressButton("lightOn");
+        remote.pressButton("setThermostat");
+        remote.undoButton("setThermostat");
     }
 }
