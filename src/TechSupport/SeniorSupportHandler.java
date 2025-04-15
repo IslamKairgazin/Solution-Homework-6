59679b72ef5ab09634bd100c6763b8b3acb03e5c
Islam Kairgazin <islam.kairgazin@narxoz.kz>
2025-04-15 15:54:46 +0500

    Added abstract class SupportHandler for chain of responsibility

---
+++ src/TechSupport/SeniorSupportHandler.java
@@ -0,0 +1,4 @@
+package TechSupport;
+
+public class SeniorSupportHandler {
+}
---
+++ src/TechSupport/SupportHandler.java
@@ -0,0 +1,11 @@
+// SupportHandler.java
+public abstract class SupportHandler {
+    protected SupportHandler nextHandler;
+
+    public SupportHandler setNext(SupportHandler handler) {
+        this.nextHandler = handler;
+        return handler;
+    }
+
+    public abstract void handle(String issue);
+}
