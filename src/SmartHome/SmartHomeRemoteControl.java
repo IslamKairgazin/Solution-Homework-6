74a19429900adeaa1e5d61566bbbf483da84c69a
Islam Kairgazin <islam.kairgazin@narxoz.kz>
2025-04-15 16:21:32 +0500

    Added SmartHomeRemoteControl class to control smart devices

--- src/SmartHome/SmartHomeRemoteControl.java
+++ src/SmartHome/SmartHomeRemoteControl.java
@@ -1,4 +1,17 @@
-package SmartHome;
+import java.util.HashMap;
 
 public class SmartHomeRemoteControl {
+    private HashMap<String, Command> commandSlots = new HashMap<>();
+
+    public void setCommand(String slot, Command command) {
+        commandSlots.put(slot, command);
+    }
+
+    public void pressButton(String slot) {
+        commandSlots.get(slot).execute();
+    }
+
+    public void undoButton(String slot) {
+        commandSlots.get(slot).undo();
+    }
 }
