36b82b6bc8ba12ebf7b95db6cab4d1d090e133a3
Islam Kairgazin <islam.kairgazin@narxoz.kz>
2025-04-15 16:16:59 +0500

    Added Thermostat class to control temperature

--- src/SmartHome/Thermostat.java
+++ src/SmartHome/Thermostat.java
@@ -1,4 +1,12 @@
-package SmartHome;
-
 public class Thermostat {
+    private int temperature;
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+        System.out.println("[Thermostat] Setting temperature to " + temperature + "°C");
+    }
+
+    public void revertToPreviousTemperature() {
+        System.out.println("[Thermostat] Reverting to previous temperature");
+    }
 }
